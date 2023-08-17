2ae5671f681c41c3885a2ada0c7611504eb0b97f
Gokul Shrikanth <gokulshrikanth99@gmail.com>
2023-08-17 09:21:21 +0530

    added path to main class

--- src/main/java/com/blaze/MainApplication.java
+++ src/main/java/com/blaze/MainApplication.java
@@ -6,5 +6,6 @@ public class MainApplication {
         public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new OAuthMiddlewareVerticle());
+        vertx.deployVerticle(new AccessTokenGeneratorVerticle());
     }
 }
